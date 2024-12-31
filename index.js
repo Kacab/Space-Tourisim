aa150095965bcd66f83ed52ae9225d7610fdc3f2
unknown <kacabibrahim@gmail.com>
2024-12-31 12:48:36 -0500

    Update destination.css

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
+
+
 const hum = document.querySelector('#Hum')
 const nav = document.querySelector('.navbar')
 let isShow = false
+const moon = document.querySelector('.moon')
+const planetsInfo = document.querySelector('.planets-info')
+const five = document.querySelector('#Five')
+const p = document.createElement('p')
+const six  =  document.querySelector('#six')
+const p2  = document.createElement('p')
+function MoonInfo(){
+    fetch('/starter-code/data.json')
+    .then((res)=>{
+       return res.json()
+
+    })
+    .then((data)=>{
+       if(data.destinations.length > 0){
+        console.log('its great')
+        const firstName = data.destinations[0].name
+        const moonDiscription =  data.destinations[0].description
+        console.log(moonDiscription)
+       p2.innerText = moonDiscription
+       p.innerText = firstName
+      five.appendChild(p)
+      six.appendChild(p2)
+        
+       }else{
+        console.log('not')
+       }
+    })
+
+    .catch((Error)=>{
+        console.log('Error34')
+    })
+}
 
 
-name1 = false
-let hidden = true
-
-
- 
+ MoonInfo()
 
 
 
