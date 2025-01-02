bf37090ffc38d3d4a394fd4f7a42b02c593ee4dd
unknown <kacabibrahim@gmail.com>
2025-01-02 13:33:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const nav = document.querySelector('.navbar')
 let isShow = false
 const moon = document.querySelector('.moon')
 const planetsInfo = document.querySelector('.planets-info')
-const five = document.querySelector('#Five')
+const moonName = document.querySelector('#moonName')
 const p = document.createElement('p')
-const six  =  document.querySelector('#six')
+const MoonDis =  document.querySelector('#MoonDis')
 const p2  = document.createElement('p')
+const Moon1 = document.querySelector('#moon1')
+const mars = document.querySelector('#mars')
+const p3 = document.createElement('p')
+const p4 =  document.createElement('p')
+const europaName = document.querySelector('#europaName')
+const marsName = document.querySelector('#marsName')
+const MarsDis = document.querySelector('#MarsDis')
+const europa = document.querySelector('#europa')
 function MoonInfo(){
     fetch('/starter-code/data.json')
     .then((res)=>{
@@ -23,8 +31,8 @@ function MoonInfo(){
         console.log(moonDiscription)
        p2.innerText = moonDiscription
        p.innerText = firstName
-      five.appendChild(p)
-      six.appendChild(p2)
+      moonName.appendChild(p)
+      moonName.appendChild(p2)
         
        }else{
         console.log('not')
@@ -37,7 +45,91 @@ function MoonInfo(){
 }
 
 
- MoonInfo()
+MoonInfo()
+
+function marsInfo(){
+    fetch('/starter-code/data.json')
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+      if(data.destinations.length > 0 ){
+      const sec = data.destinations[1].name
+      const  marsDiscription = data.destinations[1].description
+      console.log(sec)
+      console.log(marsDiscription)
+      marsName.innerText = sec
+      marsName.appendChild(p3)
+      p4.innerText = MarsDis
+      marsDiscription.appendChild(p4)
+      }else{
+        console.log('no')
+      }
+    })
+}
+
+
+
+function europaInfo(){
+    fetch('/starter-code/data.json')
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+      if(data.destinations.length > 0 ){
+      const third = data.destinations[2].name
+      const  marsDiscription = data.destinations[2].description
+      console.log(third)
+      console.log(marsDiscription)
+      europaName.innerText = third
+      europaName.appendChild()
+      }else{
+        console.log('no')
+      }
+    })
+}
+
+
+function titan(){
+    fetch('/starter-code/data.json')
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+      if(data.destinations.length > 0 ){
+      const sec = data.destinations[3].name
+      const  marsDiscription = data.destinations[3].description
+      console.log(sec)
+      console.log(marsDiscription)
+      p3.innerText = sec
+      seven.appendChild(p3)
+      }else{
+        console.log('no')
+      }
+    })
+}
+
+function remove(){
+   if(!isShow){
+    console.log('removed')
+    moonName.style.display = 'none'
+    
+    marsInfo()
+    isShow = true
+   
+   }else{
+    console.log('put back')
+    moonName.style.display = 'block'
+    isShow = false
+    console.log('put bavk')
+   }
+}
+
+
+
+
+
+ 
 
 
 
@@ -70,4 +162,22 @@ hum.addEventListener('click',function(){
 
     
    
+})
+
+Moon1.addEventListener('click',function(){
+//  MoonInfo()
+    remove()
+    // six.style.display = 'block'
+
+})
+
+
+mars.addEventListener('click',function(){
+    remove()
+})
+
+
+europa.addEventListener('click',function(){
+    // europaInfo()
+    remove()
 })
