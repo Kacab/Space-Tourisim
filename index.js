1c0010afc857b338c667bd7ba88d866294b3bd62
unknown <kacabibrahim@gmail.com>
2025-01-06 16:20:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,113 +17,144 @@ const europaName = document.querySelector('#europaName')
 const marsName = document.querySelector('#marsName')
 const MarsDis = document.querySelector('#MarsDis')
 const europa = document.querySelector('#europa')
-function MoonInfo(){
+function MoonInfo() {
+    fetch('/starter-code/data.json')
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.destinations.length > 0) {
+                const firstName = data.destinations[0].name;
+                const moonDescription = data.destinations[0].description;
+
+                // Clear the content before appending
+                moonName.innerHTML = '';
+                marsName.innerHTML = '';
+                europaName.innerHTML = '';
+
+                // Create new elements
+                const moonImage = data.destinations[0].images.png
+                 console.log(moonImage)
+                const imageMoon = document.createElement('img')
+                imageMoon.src = moonImage
+                console.log(imageMoon)
+                moon.style.display = 'block'
+                // moon.appendChild(imageMoon)
+                const moonNameElement = document.createElement('p');
+                const moonDescriptionElement = document.createElement('p');
+                moonNameElement.innerText = firstName;
+                moonDescriptionElement.innerText = moonDescription;
+
+                // Append new elements
+                moonName.appendChild(moonNameElement);
+                moonName.appendChild(moonDescriptionElement);
+
+                // Show the Moon section and hide others
+                moonName.style.display = 'block';
+                marsName.style.display = 'none';
+                
+            } else {
+                console.log('Data not found');
+            }
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+        });
+}
+
+ 
+
+function marsInfo() {
+    fetch('/starter-code/data.json')
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.destinations.length > 0) {
+                const marsNameText = data.destinations[1].name;
+                const marsDescription = data.destinations[1].description;
+
+                // Clear the content before appending
+                marsName.innerHTML = '';
+                moonName.innerHTML = '';
+                europaName.innerHTML = ''
+
+                // Create new elements
+                const marsNameElement = document.createElement('p');
+                const marsDescriptionElement = document.createElement('p');
+                marsNameElement.innerText = marsNameText;
+                marsDescriptionElement.innerText = marsDescription;
+               moon.style.display = 'none'
+              
+                // Append new elements
+                marsName.appendChild(marsNameElement);
+                marsName.appendChild(marsDescriptionElement);
+
+                // Show the Mars section and hide others
+                marsName.style.display = 'block';
+                moonName.style.display = 'none';
+
+            } else {
+                console.log('Data not found');
+            }
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+        });
+}
+
+
+function europaInfo (){
     fetch('/starter-code/data.json')
     .then((res)=>{
-       return res.json()
-
+        return res.json()
     })
     .then((data)=>{
        if(data.destinations.length > 0){
-        console.log('its great')
-        const firstName = data.destinations[0].name
-        const moonDiscription =  data.destinations[0].description
-        console.log(moonDiscription)
-       p2.innerText = moonDiscription
-       p.innerText = firstName
-      moonName.appendChild(p)
-      moonName.appendChild(p2)
+        const EuropaName = data.destinations[2].name
+        const europadis  = data.destinations[2].description
+
+        europaName.innerHTML=  '';
+        marsName.innerHTML = '';
+        moonName.innerHTML = '';
+   
+        const europanameElement = document.createElement('p')
+        const europadiscriptionEllement = document.createElement('p')
+    europadiscriptionEllement.innerText  = EuropaName
+      europanameElement.innerText = europadis
+     marsName.style.display = 'none'
+     moonName.style.display = 'none'
+      europaName.appendChild(europadiscriptionEllement) 
+        europaName.appendChild(europanameElement)
+      
         
        }else{
-        console.log('not')
+        console.log('data n0t found')
        }
+       
     })
-
-    .catch((Error)=>{
-        console.log('Error34')
-    })
-}
-
-
-MoonInfo()
-
-function marsInfo(){
-    fetch('/starter-code/data.json')
-    .then((res)=>{
-        return res.json()
-    })
-    .then((data)=>{
-      if(data.destinations.length > 0 ){
-      const sec = data.destinations[1].name
-      const  marsDiscription = data.destinations[1].description
-      console.log(sec)
-      console.log(marsDiscription)
-      marsName.innerText = sec
-      marsName.appendChild(p3)
-      p4.innerText = MarsDis
-      marsDiscription.appendChild(p4)
-      }else{
-        console.log('no')
-      }
+    .catch((error)=>{
+        console.log(error,'no data foundd')
     })
 }
 
 
+// function titan(){
+//     fetch('/starter-code/data.json')
+//     .then((res)=>{
+//         return res.json()
+//     })
+//     .then((data)=>{
+//       if(data.destinations.length > 0 ){
+//       const sec = data.destinations[3].name
+//       const  marsDiscription = data.destinations[3].description
+//       console.log(sec)
+//       console.log(marsDiscription)
+//       p3.innerText = sec
+//       seven.appendChild(p3)
+//       }else{
+//         console.log('no')
+//       }
+//     })
+// }
 
-function europaInfo(){
-    fetch('/starter-code/data.json')
-    .then((res)=>{
-        return res.json()
-    })
-    .then((data)=>{
-      if(data.destinations.length > 0 ){
-      const third = data.destinations[2].name
-      const  marsDiscription = data.destinations[2].description
-      console.log(third)
-      console.log(marsDiscription)
-      europaName.innerText = third
-      europaName.appendChild()
-      }else{
-        console.log('no')
-      }
-    })
-}
 
-
-function titan(){
-    fetch('/starter-code/data.json')
-    .then((res)=>{
-        return res.json()
-    })
-    .then((data)=>{
-      if(data.destinations.length > 0 ){
-      const sec = data.destinations[3].name
-      const  marsDiscription = data.destinations[3].description
-      console.log(sec)
-      console.log(marsDiscription)
-      p3.innerText = sec
-      seven.appendChild(p3)
-      }else{
-        console.log('no')
-      }
-    })
-}
-
-function remove(){
-   if(!isShow){
-    console.log('removed')
-    moonName.style.display = 'none'
-    
-    marsInfo()
-    isShow = true
-   
-   }else{
-    console.log('put back')
-    moonName.style.display = 'block'
-    isShow = false
-    console.log('put bavk')
-   }
-}
 
 
 
@@ -165,19 +196,19 @@ hum.addEventListener('click',function(){
 })
 
 Moon1.addEventListener('click',function(){
-//  MoonInfo()
-    remove()
-    // six.style.display = 'block'
-
+ MoonInfo()
+  
+    //six.style.display = 'block'
+    
 })
 
 
 mars.addEventListener('click',function(){
-    remove()
+   marsInfo()
 })
 
 
 europa.addEventListener('click',function(){
-    // europaInfo()
-    remove()
+     europaInfo()
+    
 })
